b7d99f7ba33c4fbaf1c27ee7c3312ec944f7ade4
Nagireddy Roddam <roddamnagireddy1@gmail.com>
2026-06-18 09:39:45 +0530

    react todo app with railway backend

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [task, setTask] = useState([]);
+  const [input, setInput] = useState('');
+
+  function deleteTask(index) {
+    setTask(task.filter((t, i) => i !== index));
+  }
+
+  useEffect(() => {
+    fetch('https://my-back-end-production.up.railway.app/tasks')
+      .then(res => res.json())
+      .then(data => setTask(data));
+  }, []);
+  function addTask(){
+    fetch('https://my-back-end-production.up.railway.app/tasks', {
+      method:'post',
+      headers:{'Content-Type': 'application/json'},
+      body: JSON.stringify({task: input})
+    })
+    .then(res => res.json())
+    .then(data =>{
+      setTask(data);
+      setInput(''); 
+
+    });
+  }
+  function deleteTask(id){
+    fetch(`https://my-back-end-production.up.railway.app/tasks/${id}`, {
+      method:'DELETE',
+    })
+    .then(res => res.json())
+    .then(data => setTask(data));
+  }
+
+  return(
+    <div>
+      <h1>My Todo List</h1>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a task"
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {task.map((task, index) => (
+          <li key={index}>
+            {task.task}
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App;
